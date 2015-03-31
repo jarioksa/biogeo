a2e17b05696783e394f16711150c3c2990c1465a
Jari Oksanen <jari.oksanen@oulu.fi>
2015-03-31 13:35:18 +0300

    Adjust for lengths of months

--- R/PEThorn.R
+++ R/PEThorn.R
@@ -11,10 +11,13 @@
     function (temp, lat) 
 {
     tandecl <- c(-0.384925, -0.22606, -0.02156, 0.182725, 0.35105, 0.432297, 0.388417, 0.242569, 0.048042, -0.157378, -0.337603, -0.430845)
+    ## Thornthwaite uses 30-days months (Thornthwaite 1948, p. 94). 
+    monlen <- c(31,28.24,31,30,31,30,31,31,30,31,30,31)/30
     take <- temp>0
     if (sum(take) == 0)
         return(0)
     PE <- numeric(sum(take))
+    monlen <- monlen[take]
     temp <- temp[take]
     tandecl <- tandecl[take]
     hot <- temp >= 26.5
@@ -23,11 +26,13 @@
     if (any(hot))
         PE[hot] <- -415.855 + 32.244*temp[hot] - 0.43253*temp[hot]^2
     if (any(!hot)) {
+        ## c is daylength per 12h
         c <- -tan(lat/180*pi)*tandecl[!hot]
         c <- acos(pmin(1, pmax(-1, c)))/pi*2
+        ## Thornthwaite 1948, eq. 9 and around
         Ival <- sum((temp/5)^1.514)
         A <- 6.75e-7*Ival^3 - 7.71e-5*Ival^2 + 1.792e-2*Ival + 0.49239
         PE[!hot] <- 16*c*(10*temp[!hot]/Ival)^A
     }
-    sum(PE)
+    sum(monlen*PE)
 }
