8d773e0a3e21e26068618cd6e9947c8e778f7ad3
Jari Oksanen <jari.oksanen@oulu.fi>
2015-03-31 12:46:19 +0300

    Add Thornthwaite's Potential Evapotranspiration

---
+++ R/PEThorn.R
@@ -0,0 +1,22 @@
+`PEThorn` <-
+    function (temp, lat) 
+{
+    tandecl <- c(-0.384925, -0.22606, -0.02156, 0.182725, 0.35105, 0.432297, 0.388417, 0.242569, 0.048042, -0.157378, -0.337603, -0.430845)
+    take <- temp>0
+    if (sum(take) == 0)
+        return(0)
+    PE <- numeric(sum(take))
+    temp <- temp[take]
+    tandecl <- tandecl[take]
+    hot <- temp > 26.5
+    if (any(hot))
+        PE[hot] <- -415.84 + 32.24*temp[hot] - 0.435*temp[hot]^2
+    if (any(!hot)) {
+        c <- -tan(lat/180*pi)*tandecl[!hot]
+        c <- acos(pmin(1, pmax(-1, c)))/pi*2
+        Ival <- sum((temp/5)^1.514)
+        A <- 6.75e-7*Ival^3 - 7.71e-5*Ival^2 + 1.79e-2*Ival + 0.492
+        PE[!hot] <- 16*c*(10*temp[!hot]/Ival)^A
+    }
+    sum(PE)
+}
