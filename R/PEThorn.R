3f9b7b4c0fe625283e18f1ff795b602389d0cbe5
Jari Oksanen <jari.oksanen@oulu.fi>
2015-03-31 15:58:06 +0300

    More complete documentation of PEThorn.

--- R/PEThorn.R
+++ R/PEThorn.R
@@ -1,5 +1,35 @@
 #' Thornthwaite's Potential Evapotranspiration
 #'
+#' Function estimates the Potential Evapotranspiration (PET) from
+#' monthly mean temperatures following Thornthwaite (1948). The
+#' original equations were calibrated for U.S.A., and were the first
+#' attempt at finding PET from readily available meteorological data.
+#'
+#' Function \code{\link[SPEI]{thornthwaite}} (package \pkg{SPEI})
+#' provides an alternative implementation, with two important
+#' differences: (1) Thornthwaite (1948) suggested that the basic
+#' equations should be used only up to 26.5 degrees, and then we
+#' should use to a parabolic model which was given in tabular
+#' form. The \code{PEThorn} uses estimated polynomial coefficients of
+#' this parabola for monthly temperatures over 26.5 degrees, whereas
+#' \pkg{SPEI} package uses the same basic equations which give much
+#' higher values. (2) In winter-seasonal climates, the basic equations
+#' can actually give increased PET with cooler climate and shorter
+#' period of positive temperatures. The \code{PEThorn} function
+#' adjusts the model coefficient (\eqn{I}) for the length of the
+#' period with positive temperatures. This is unpublished, but seems
+#' to improve the estimates in near-arctic conditions.
+#'
+#' @seealso Package \pkg{SPEI} has a wider choice of indices of PET,
+#' among them Hargreaves index and its modification
+#' (\code{\link[SPEI]{hargreaves}}) which is also suitable for
+#' standard meteorological data, and has been calibrated globally.
+#'
+#' @author Jari Oksanen
+#' @return Annual potential evapotranspiration in mm. 
+#' @examples
+#' PEThorn(c(-9.6,-9.3,-4.8,1.4,7.8,13.5,16.5,14.1,8.9,3.3,-2.8,-7.1),
+#'    lat=65)
 #' @references Thornthwaite C.W. (1948) An approach toward a rational
 #' classification of climate. \emph{Geographical Review} 38, 55--94.
 #'
